bfdf57b2f0b56d7d32e99ca2a0dd99ed744b3a31
kr4600 <kr4600@users.noreply.github.com>
2019-04-10 22:59:07 +0200

    sito do liczb
    - w pełni funkconalne sito eratostenesa
    - pętla do testów
    - komentarz zbędnych partii kodu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random as r
 from math import ceil, sqrt
-
+'''
 while(1):
     # ostatnia pierwsza przed liczbą będzie podana
     liczba = input('podaj liczbe: ')
@@ -11,5 +11,23 @@ while(1):
         print('nie podano liczby')
 
 # ostatni wyraz tablicy
-lastTab = ceil(sqrt(liczba)) ** 2
+granica = ceil(sqrt(liczba))
+lastTab = granica ** 2
+'''
+lastTab = 100
+granica = 10
 print(lastTab)
+
+#sito eratostenesa init
+tab = [True] * (lastTab+1)
+tab[0] = False
+tab[1] = False
+#samo sito
+for i in range(2, granica):
+    for j in range(i*2,len(tab),i):
+        tab[j] = False
+
+
+for i in range(len(tab)):
+  if tab[i]==True:
+      print(i)
