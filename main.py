6c367995dcc75f9851f71dffe6321249a55fb74d
kr4600 <kr4600@users.noreply.github.com>
2019-04-11 16:57:13 +0200

    wyznaczanie e
    - zmiana wielkości tablicy na rzecz wykorzystania pierwszych jako liczba e
    - wypisywanie kluczowych wartości
    - wybieranie poprawnego e

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from math import ceil, sqrt
 from random import choice, randrange
 
+def pierwsze(od, do, coIle, dlugosc):
+    pierwsze = []
+    for i in range(od, do, coIle):
+        if tab[i] is True and len(pierwsze) < dlugosc:
+            print(i)
+            pierwsze.append(i)
+    return pierwsze;
 '''
 while(1):
     # ostatnia pierwsza przed liczbą będzie podana
@@ -15,9 +22,10 @@ while(1):
 granica = ceil(sqrt(liczba))
 lastTab = granica ** 2
 '''
-lastTab = 100
-granica = 10
-print(lastTab)
+liczba = 400
+lastTab = int(400*2)
+granica = int(20*2)
+print(f'ostani {lastTab}')
 
 # sito eratostenesa init
 tab = [True] * (lastTab + 1)
@@ -28,14 +36,26 @@ for i in range(2, granica):
     for j in range(i * 2, len(tab), i):
         tab[j] = False
 
+pierwszeP = pierwsze(liczba, 2, -1, 10)
+print(pierwszeP)
 
-pierwsze = []
-for i in range(len(tab) - 1, 2, -1):
-    if tab[i] is True and len(pierwsze) < 10:
-        print(i)
-        pierwsze.append(i)
+p1 = choice(pierwszeP)
+pierwszeP.remove(p1)
+p2 = choice(pierwszeP)
+print(f'{p1} i {p2}')
+print(f'pierwszeP {pierwszeP}')
+n = p1*p2
 
-p1 = choice(pierwsze)
-p2 = choice(pierwsze)
+# wibieranie e
+# <max(p1,p2)+1;n-1>
+fi = (p1-1)*(p2-1)
+print(f'fi {fi}')
+# "len(tab) - 2" bo -1 od długości, i -1 z przedziału
+# "max(p1, p2)+2" bo 2 nigdy nie będzie większe, a wszystkieinne pierwsze
+# są nieparzyste
 
-print(f'{r1} i {r2}')
+# TODO zwiekszy dlugosc (teraz 10)
+pierwszeE = pierwsze(max(p1, p2)+2, len(tab) - 2, 2, 10)
+print(f'pierwszeE {pierwszeE}')
+e = choice(pierwszeE)
+print(f'e = {e}')
